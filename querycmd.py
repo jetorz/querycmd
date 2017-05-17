77f5061ff8a975378ac8fa9082fd49b93d1aaac9
jetorz <jetorz@gmail.com>
2017-05-18 07:51:08 +0800

    Add bd command for Baidu

--- querycmd.py
+++ querycmd.py
@@ -1,7 +1,7 @@
 import sys
 import webbrowser as wb
 
-cmdlist = ['db']
+cmdlist = ['db','bd']
 
 cmd = sys.argv[1]
 
@@ -15,13 +15,12 @@ key = ""
 for r in rest:
     key += str(r) + ' '
 
-# print(sys.argv[:])
-# print(cmd)
-# print(key)
-
 # Douban book query.
 if cmd == 'db':
     url = r'https://book.douban.com/subject_search?search_text=' + key + r'&cat=1003'
+    wb.open_new_tab(url)
+elif cmd =='bd':
+    url = r'https://www.baidu.com/s?wd=' + key
     wb.open_new_tab(url)
 else:
     print('Unkonw command %s. Contact me for detail.' % cmd)
