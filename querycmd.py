7a163922ca6ab4ba9a4af64f5f29c5009b9e1bd4
jetorz <jetorz@gmail.com>
2017-05-23 08:07:53 +0800

    change search engine to setting files instead of hardcode in script.

--- querycmd.py
+++ querycmd.py
@@ -1,26 +1,33 @@
 import sys
 import webbrowser as wb
 
-cmdlist = ['db','bd']
-
 cmd = sys.argv[1]
-
-if cmd not in cmdlist:
-    print('Unkonw command %s. Contact me for detail.' % cmd)
-    out = input()
-    sys.exit(0)
-
 rest = sys.argv[2:]
 key = ""
 for r in rest:
     key += str(r) + ' '
 
-# Douban book query.
-if cmd == 'db':
-    url = r'https://book.douban.com/subject_search?search_text=' + key + r'&cat=1003'
-    wb.open_new_tab(url)
-elif cmd =='bd':
-    url = r'https://www.baidu.com/s?wd=' + key
-    wb.open_new_tab(url)
-else:
-    print('Unkonw command %s. Contact me for detail.' % cmd)
+cmdfile = open('engine.DongTalks', 'r',encoding='utf-8')
+
+cmddict = {}
+for line in cmdfile.readlines():
+    if line.startswith('#'):
+        pass
+    else:
+        engine = line.split(sep='|')
+        before = engine[1].strip()
+        if len(engine) == 3:
+            after = ''
+        else:
+            after = engine[3].strip()
+        cmddict[engine[0].strip()] = before + key + after
+
+if cmd not in cmddict.keys():
+    print('Unkonw command "%s". Contact me for detail.' % cmd)
+    print('You know how to find me, do you? ;)')
+    out = input()
+    sys.exit(0)
+
+print(cmddict[cmd])
+
+wb.open_new_tab(cmddict[cmd])
