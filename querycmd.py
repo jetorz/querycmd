85ac162be22c64e000eb57d3639542dff0763044
jetorz <jetorz@gmail.com>
2017-06-15 06:34:52 +0800

    Add command not for search, such as 'about' and 'dt'.

--- querycmd.py
+++ querycmd.py
@@ -1,33 +1,59 @@
-import sys
+import sys, math
 import webbrowser as wb
 
-cmd = sys.argv[1]
+def about():
+    wb.open_new_tab('about.html')
+
+def dt():
+    a = float(input('输入年投资额（元）：'))
+    x = input('输入年化收益率百分比(8)：')
+    print(x)
+    if len(x) == 0:
+        x=0.08
+    else:
+        x = float(x)/100
+    print(x)
+    n = int(input('输入定投年数（年）：')) * 1.0
+    M = a*(1+x)*(-1+pow((1+x),n))/x
+    print('定投结束后，总金额为：%.2f。' % M)
+    input('Press any key to continue...')
+
+cmd = str(sys.argv[1])
 rest = sys.argv[2:]
-key = ""
+query = ""
 for r in rest:
-    key += str(r) + ' '
+    query += str(r) + ' '
 
 cmdfile = open('engine.DongTalks', 'r',encoding='utf-8')
 
-cmddict = {}
+# built-in functions
+cmddict = {'about': about, 'dt':dt}
+
 for line in cmdfile.readlines():
-    if line.startswith('#'):
+    if line.startswith('#') or line.startswith('\n'):
         pass
     else:
         engine = line.split(sep='|')
-        before = engine[1].strip()
+        for i in range(0, len(engine)):
+            engine[i] = engine[i].strip()
         if len(engine) == 3:
+            before = engine[1].strip()
             after = ''
         else:
-            after = engine[3].strip()
-        cmddict[engine[0].strip()] = before + key + after
+            after = engine[3]
+        searchurl = before + query + after
+        cmddict[engine[0]] = searchurl
+
+print(cmd)
+print(cmddict[cmd])
 
 if cmd not in cmddict.keys():
     print('Unkonw command "%s". Contact me for detail.' % cmd)
     print('You know how to find me, do you? ;)')
     out = input()
     sys.exit(0)
-
-print(cmddict[cmd])
-
-wb.open_new_tab(cmddict[cmd])
+else:
+    if type(cmddict[cmd]) == str:
+        wb.open_new_tab(cmddict[cmd])
+    else:
+        cmddict[cmd]()
