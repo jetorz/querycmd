17627ae66e30ecd901e4a3da8733cab953e433c6
Zhang Lidong <jetorz@gmail.com>
2019-10-07 21:05:33 +0800

    Add google translate as gc and ge

--- querycmd.py
+++ querycmd.py
@@ -1,6 +1,18 @@
 import sys, math, os, platform, subprocess
 import webbrowser as wb
 
+DEBUGMODE = False
+
+def dPrint(x, pairs=locals().items()):
+    if DEBUGMODE == False:
+        return
+    varlist = []
+    for (k, v) in pairs:
+        if v == x:
+            varlist.append((k, v))
+    dbg = open('dbg.txt', mode='a', encoding='utf8')
+    print('%s => %s' % (varlist[0][0], x), file=dbg)
+
 def PraseEngine(cmdfile):
     cmddict = {}
     for line in cmdfile:
@@ -15,27 +27,29 @@ def PraseEngine(cmdfile):
     return cmddict
 
 if __name__ == "__main__":
-    dbg = open('dbg.txt', 'w', encoding='utf8')
+    if DEBUGMODE == True:
+        dbg = open('dbg.txt', mode='w', encoding='utf8')
+        dbg.close()
+
+    dbg = open('dbg.txt', 'a', encoding='utf8')
     argv = sys.argv
-    print('argv=> %s' % argv, file=dbg)
+    dPrint(argv)
     cmdfile = open('engine.txt', 'r', encoding='utf-8')
     engine = PraseEngine(cmdfile)
-    print('engine => %s.' % engine, file=dbg)
+    dPrint(engine)
 
     cmd = argv[1].lower()
-    print('cmd=>%s ' % cmd, file=dbg)
+    dPrint(cmd)
     query = ''
     if cmd in engine.keys():
-        print(cmd + ' is in engine.', file=dbg)
         for i in range(2, len(argv)):
             query = query + argv[i] + ' '
         surl = engine[cmd]['before'] + query + engine[cmd]['after']
-        print('seach url is:' + surl, file=dbg)
+        dPrint(surl)
     else:
-        print(cmd + ' is not in engine.', file=dbg)
         for i in range(1, len(argv)):
             query = query + argv[i] + ' '
         surl = engine['g']['before'] + query
+        dPrint(surl)
 
     wb.open_new_tab(surl)
-    dbg.close()
